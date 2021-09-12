9f8f3f62e3c0d673da5c0f6311c1a266d99c6ba0
Lowpoly <sky59835@gmail.com>
2021-09-12 18:15:56 +0900

    에러 메세지에 odbc error state 코드 추가 출력

--- DBConnectionDLL/CustomString.h
+++ DBConnectionDLL/CustomString.h
@@ -1,39 +1,45 @@
 #pragma once
 #include "ExportDefine.h"
 
-class DBCONNECTIONDLL_EXPORTS_DECLSPEC CustomString
+namespace NetworkCommon
 {
-public:
-	CustomString();
-	CustomString(const char* value);
-	CustomString(const wchar_t* value);
-	CustomString(const CustomString& other);
-	~CustomString();
+	namespace DBConnection
+	{
+		class DBCONNECTIONDLL_EXPORTS_DECLSPEC CustomString
+		{
+		public:
+			CustomString();
+			CustomString(const char* value);
+			CustomString(const wchar_t* value);
+			CustomString(const CustomString& other);
+			~CustomString();
 
-	CustomString& operator=(const char* value);
-	CustomString& operator+=(const char* value);
-	CustomString& operator=(const wchar_t* value);
-	CustomString& operator+=(const wchar_t* value);
-	CustomString& operator=(const CustomString& other);
-	operator const char* ();
-	
-private:
-	bool Write(const char* desc, size_t length);
-	bool Write(const wchar_t* desc, size_t length);
-	void Clear();
-public:
-	size_t Capacity();
-	const char* c_str();
-	const char* data();
-	bool Empty();
-	size_t Size();
-	void Resize(size_t length);
-private:
-	static const int DEFAULT_SIZE = 128;
+			CustomString& operator=(const char* value);
+			CustomString& operator+=(const char* value);
+			CustomString& operator=(const wchar_t* value);
+			CustomString& operator+=(const wchar_t* value);
+			CustomString& operator=(const CustomString& other);
+			operator const char* ();
 
-	size_t max_size;
-	size_t write_size;
-	char* m_Buff;
-};
+		private:
+			bool Write(const char* desc, size_t length);
+			bool Write(const wchar_t* desc, size_t length);
+			void Clear();
+		public:
+			size_t Capacity();
+			const char* c_str();
+			const char* data();
+			void ToWchar();
+			bool Empty();
+			size_t Size();
+			void Resize(size_t length);
+			size_t Max();
+		private:
+			static const int DEFAULT_SIZE = 128;
 
-
+			size_t max_size;
+			size_t write_size;
+			char* m_Buff;
+		};
+	}
+}
